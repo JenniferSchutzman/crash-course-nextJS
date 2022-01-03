fab1cf4e00cb5555e34056153083eed233b8d760
Jenny Schutzman <jenny.schutzman@ratequote.com>
2022-01-03 13:42:02 -0500

    getStaticPaths with getStaticProps

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const article = ({ article }) => {
+  //   const router = useRouter();
+  //   const { id } = router.query;
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/"> Go Back</Link>
+    </>
+  );
+};
+
+// everytime we go to a page we are activating this getServerSideProps to get the data
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+// now we'll do it in with getStaticProps and getStaticPaths to dynamically change the paths
+// this will make it much faster bc generated at build time and can be used in a static site
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+
+  const ids = articles.map((article) => article.id);
+
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default article;
